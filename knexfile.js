ccc99c82dc765d8e6937c7aa55acead9f91496fe
TTPOCTOLikes <dyagovets.vova2@gmail.com>
2020-08-25 12:26:14 +0300

    test adding postgresql heroku remote database. #3

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,3 @@
-// Update with your config settings.
 module.exports = {
     development: {
         client: 'sqlite3',
@@ -15,10 +14,8 @@ module.exports = {
     },
 
     production: {
-        client: 'postgresql',
-        connection: {
-            database: process.env.DATABASE_URL
-        },
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
         pool: {
             min: 2,
             max: 10
@@ -30,21 +27,4 @@ module.exports = {
             directory: "./knex_configurations/seeds"
         },
     }
-
-    // staging: {
-    //   client: 'postgresql',
-    //   connection: {
-    //     database: 'my_db',
-    //     user:     'username',
-    //     password: 'password'
-    //   },
-    //   pool: {
-    //     min: 2,
-    //     max: 10
-    //   },
-    //   migrations: {
-    //     tableName: 'knex_migrations'
-    //   }
-    // },
-    //
 };
